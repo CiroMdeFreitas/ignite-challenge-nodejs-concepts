3fee248ab292208cb3176d3ada3b2842c2c5ba05
Ciro Freitas <cirofreitas1995@gmail.com>
2022-01-19 12:08:09 -0300

    Checks if user exists

--- src/index.js
+++ src/index.js
@@ -11,7 +11,14 @@ app.use(express.json());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { id } = request.header;
+  
+  const user = users.find(user => user.id == id);
+  if(!user)
+    return response.status(404).json({ error: "User not found!" });
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
